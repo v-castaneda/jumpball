94fa2fe8e50c0b638fc8655b25b160f4cf3beeb8
Joseph Yoon <jyoonopop@gmail.com>
2022-10-20 16:43:30 -0400

    added map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import Default from './components/Default'
+import React from 'react';
+import { MapContainer, TileLayer, Polygon } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import './App.css'
 
-function App() {
+const center = [40.97292346150078, -74.03187326608914]
+
+export default function App() {
   return (
-    <div className="App">
-      <Default/>
-    </div>
-  );
-}
-
-export default App;
+    <MapContainer
+      center={center}
+      zoom={10}
+      style={{ width: '100vw', height: '100vh' }}
+    >
+    <TileLayer
+      url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=xfaxWdmYiFlarNYCFP6w'
+      attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    />
+    <Polygon
+      
+    />
+    </MapContainer>
+  )
+};
