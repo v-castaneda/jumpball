b26e529e9155650bd57622d9d64f9594becc40b4
unknown <jessespeks@hotmail.com>
2022-10-19 21:26:05 -0400

    attempted usage of leaflet map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Default from './components/Default'
 
--- src/components/Default.js
+++ src/components/Default.js
@@ -1,3 +1,9 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup
+} from 'https://cdn.esm.sh/react-leaflet'
 const React = require('react')
 
 function Default(html) {
@@ -13,7 +19,19 @@ function Default(html) {
      crossorigin=""></script>
     </head>
     <body>
-      <div id="map" style={{height: "180px"}}></div>
+      <div id="map" style={{height: "180px"}}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+      </div>
     </body>
     </html>
   )
